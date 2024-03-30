14b05cfaa9f40cec9c97cb0c6e312d66fc5b31de
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2024-03-30 20:40:03 +0500

    Implemented Home and Message Fragment[using Recycler View]

---
+++ app/src/main/java/com/example/securetext/Adapter/MessageAdapter.java
@@ -0,0 +1,126 @@
+package com.example.securetext.Adapter;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.securetext.Database.RoomDB;
+import com.example.securetext.Model.Message;
+import com.example.securetext.R;
+import com.example.securetext.Utility.BDUtility;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.myViewHolder> {
+
+
+    List<Message> messageList;
+    LayoutInflater layoutInflater;
+    RoomDB database;
+
+    public MessageAdapter(List<Message> messageList, Context context, RoomDB database) {
+        this.messageList = messageList;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.database = database;
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.item_message, parent, false);
+        return new myViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+        holder.txtPlain.setText(messageList.get(position).getPtext());
+        holder.txtEncrypted.setText(messageList.get(position).getEtext());
+        holder.txtDate.setText(messageList.get(position).getCreationTime().toString());
+
+
+        holder.btnCopyToClipboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BDUtility.setClipBoard(v.getContext(), messageList.get(position).getEtext());
+                Toast.makeText(v.getContext(), "Copies to clipboard", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(v.getContext())
+                        .setTitle("Delete!!")
+                        .setMessage("Are you sure?")
+                        .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                database.mainDao().delete(messageList.get(position));
+                                messageList.remove(messageList.get(position));
+                                notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setIcon(R.drawable.ic_delete)
+                        .show();
+
+            }
+        });
+
+        holder.btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View inflater) {
+                // I gave name inflater form myself before this it was just v
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, messageList.get(position).getEtext());
+                intent.setType("text/plain");
+
+                if (intent.resolveActivity(inflater.getContext().getPackageManager()) != null) {
+                    inflater.getContext().startActivity(intent);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+    public static class myViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtPlain, txtEncrypted, txtDate;
+        ImageButton btnShare, btnCopyToClipboard, btnDelete;
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtPlain = itemView.findViewById(R.id.txtPlainText);
+            txtEncrypted = itemView.findViewById(R.id.txtEncryptedText);
+            txtDate = itemView.findViewById(R.id.txtCreationTime);
+
+            btnShare = itemView.findViewById(R.id.btnShare);
+            btnCopyToClipboard = itemView.findViewById(R.id.btnCopyToClipboard);
+            btnDelete = itemView.findViewById(R.id.btnDelete);
+
+
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/securetext/fragments/HomeFragment.java
+++ app/src/main/java/com/example/securetext/fragments/HomeFragment.java
@@ -7,60 +7,113 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.securetext.Database.RoomDB;
+import com.example.securetext.EncryptDecrypt;
+import com.example.securetext.Model.Key;
+import com.example.securetext.Model.Message;
 import com.example.securetext.R;
+import com.example.securetext.Utility.BDUtility;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Date;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private MaterialButton btnEncrypt, btnDecrypt;
+    private TextInputEditText textEncrypt, textDecrypt;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+    RoomDB database;
+    String key;
+
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        database = RoomDB.getInstance(getActivity());
+        textEncrypt = view.findViewById(R.id.textEncrypt);
+        textDecrypt = view.findViewById(R.id.textDecrypt);
+        btnEncrypt = view.findViewById(R.id.btnEncrypt);
+        btnDecrypt = view.findViewById(R.id.btnDecrypt);
+
+
+        btnEncrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = textEncrypt.getText().toString();
+                if (value != null && !value.isEmpty()) {
+                    key = database.keyDao().getAllKey().get(0).getKey();
+                    String encryptedText = EncryptDecrypt.encrypt(textEncrypt.getText().toString(), key);
+                    textDecrypt.setText(encryptedText);
+
+                    List<Key> keyList = database.keyDao().getAllKey();
+                    Boolean saveMessage = keyList.get(0).getMessagebackup();
+
+                    if (saveMessage) {
+                        saveEncryptedMessage(encryptedText, textEncrypt.getText().toString());
+                    }
+                    BDUtility.setClipBoard(getContext(), textDecrypt.getText().toString());
+
+                    textEncrypt.getText().clear();
+                    Toast.makeText(getContext(), "Encrypted text copied to clipboard", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getContext(), "Field empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+        btnDecrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+
+                    String value = textDecrypt.getText().toString();
+                    if (value != null && !value.isEmpty()) {
+                        key = database.keyDao().getAllKey().get(0).getKey();
+                        String decryptedText = EncryptDecrypt.decrypt(value, key);
+                        textEncrypt.setText(decryptedText);
+                        textDecrypt.getText().clear();
+
+                        BDUtility.setClipBoard(getContext(), textEncrypt.getText().toString());
+                        Toast.makeText(getContext(), "Decrypted text copied to clipboard", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(getContext(), "Field empty", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                    Toast.makeText(getContext(), "Invalid encrypted data", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+        return view;
+    }
+
+
+    private void saveEncryptedMessage(String eText, String pText) {
+        Message message = new Message();
+        message.setEtext(eText);
+        message.setPtext(pText);
+        message.setCreationTime(new Date());
+        database.mainDao().saveItem(message);
     }
 }
